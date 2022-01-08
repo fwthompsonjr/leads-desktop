664aa81dc39c7c9f4ecd7704830ba4e0127fd94f
Frank Thompson <Frank.Thompson@wolterskluwer.com>
2022-01-08 11:48:26 -0600

    hcc case download routine

---
+++ Thompson.RecordSearch.Utility/Dto/HccOptionDto.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thompson.RecordSearch.Utility.Dto
+{
+    public class HccOptionDto
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("labels")]
+        public List<string> Labels { get; set; }
+
+        [JsonProperty("index")]
+        public int? Index { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("values")]
+        public List<string> Values { get; set; }
+
+    }
+}
--- UnitTests/Thompson.RecordSearch.Utility.UnitTests/Db/DataataProcessTest.cs
+++ UnitTests/Thompson.RecordSearch.Utility.UnitTests/Db/DataataProcessTest.cs
@@ -7,7 +7,7 @@ using Thompson.RecordSearch.Utility.Db;
 namespace Thompson.RecordSearch.Utility.UnitTests.Db
 {
     [TestClass]
-    public class DownloadDataProcessTest
+    public class DataataProcessTest
     {
         [TestMethod]
         public async Task CanExecute_Download()
@@ -20,6 +20,28 @@ namespace Thompson.RecordSearch.Utility.UnitTests.Db
             var process = new DownloadDataProcess();
             // The Progress<T> constructor captures our UI context,
             //  so the lambda will be run on the UI thread.
+            var progress = new Progress<HccProcess>(percent =>
+            {
+                reported = percent;
+            });
+
+            // Processing is run on the thread pool.
+            await Task.Run(() => process.Process(progress));
+            Assert.IsNotNull(reported);
+        }
+
+
+        [TestMethod]
+        public async Task CanExecute_DownloadCases()
+        {
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+                Assert.Inconclusive("This method to be executed in debug mode only.");
+            }
+            HccProcess reported = default;
+            var process = new DownloadCaseProcess();
+            // The Progress<T> constructor captures our UI context,
+            //  so the lambda will be run on the UI threa-
             var progress = new Progress<HccProcess>(percent =>
             {
                 reported = percent;
