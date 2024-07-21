2d8cab1e1b6300669bec3cf4e43c019a59a306ed
Frank Thompson <frank.thompson.jr@gmail.com>
2024-07-21 04:12:33 -0500

    feat: update version number to 2.7.2

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+## Problem:
+
+When a user attempts to perform any county search
+The process will not open the generated Excel output.
+
+## Resolution:
+- [x] Confirm excel file generation
+- [x] Create excal file to open generated files
+- [x] Test behavior for all counties
+- [x] Update package to generate release
---
+++ build-release-notes.ps1
@@ -0,0 +1,34 @@
+function getVersionDetails($jsfile){
+    if ( [System.IO.File]::Exists( $jsfile ) -eq $false ) { return $null; }
+    $js = [System.IO.File]::ReadAllText( $jsfile ) | ConvertFrom-Json;
+    return $js[0];
+}
+
+function updateGitVariable($key, $setting)
+{
+    try {
+        $assignment="$key=$setting"
+        echo $assignment | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+    } catch {
+    }
+}
+
+$basefolder = [System.IO.Path]::GetDirectoryName( $MyInvocation.MyCommand.Path );
+$versionFile = [System.IO.Path]::Combine( $basefolder, "setup\setup-version.txt" ); 
+$changelog = [System.IO.Path]::Combine( $basefolder, "CHANGELOG.md" ); 
+if ( [System.IO.File]::Exists( $versionFile ) -eq $false ) { return; }
+if ( [System.IO.File]::Exists( $changelog ) -eq $true )
+{ 
+   [System.IO.File]::Delete( $changelog ) 
+}
+$version = getVersionDetails -jsfile $versionFile
+if ($null -eq $version ) { return; }
+$versionDescription = [string]::Join([Environment]::NewLine, $version.description)
+[System.IO.File]::WriteAllText( $changelog, $versionDescription);
+$versionTitle = $version.title
+$setupZip = "legal-lead-install-$($version.id).zip"
+$excelZip = "legal-lead-excel-addin-$($version.id).zip"
+
+updateGitVariable -key "RELEASE_TITLE" -setting $versionTitle
+updateGitVariable -key "ASSET_SETUP_FILE" -setting $setupZip
+updateGitVariable -key "ASSET_EXCEL_FILE" -setting $excelZip
