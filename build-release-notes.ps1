55db9858fef9842e7ac2392eec426d3c60161286
Frank Thompson <frank.thompson.jr@gmail.com>
2024-07-21 04:28:30 -0500

    fix: write asset name variables to environment

--- build-release-notes.ps1
+++ build-release-notes.ps1
@@ -4,15 +4,6 @@ function getVersionDetails($jsfile){
     return $js[0];
 }
 
-function updateGitVariable($key, $setting)
-{
-    try {
-        $assignment="$key=$setting"
-        echo $assignment | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-    } catch {
-    }
-}
-
 $basefolder = [System.IO.Path]::GetDirectoryName( $MyInvocation.MyCommand.Path );
 $versionFile = [System.IO.Path]::Combine( $basefolder, "setup\setup-version.txt" ); 
 $changelog = [System.IO.Path]::Combine( $basefolder, "CHANGELOG.md" ); 
@@ -25,14 +16,3 @@ $version = getVersionDetails -jsfile $versionFile
 if ($null -eq $version ) { return; }
 $versionDescription = [string]::Join([Environment]::NewLine, $version.description)
 [System.IO.File]::WriteAllText( $changelog, $versionDescription);
-$versionTitle = $version.title
-$setupZip = [System.IO.Path]::Combine( $basefolder,"legal-lead-install-$($version.id).zip")
-$excelZip = [System.IO.Path]::Combine( $basefolder,"legal-lead-excel-addin-$($version.id).zip")
-
-    echo "ASSET_SETUP_FILE=$setupZip" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-    echo "ASSET_EXCEL_FILE=$excelZip" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-<#
-updateGitVariable -key "RELEASE_TITLE" -setting $versionTitle
-updateGitVariable -key "ASSET_SETUP_FILE" -setting $setupZip
-updateGitVariable -key "ASSET_EXCEL_FILE" -setting $excelZip
-#>
