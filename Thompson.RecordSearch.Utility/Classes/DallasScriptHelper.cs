241dec2278d7beca84ae48436acd077f2ae1a9af
Frank Thompson <frank.thompson.jr@gmail.com>
2024-09-29 15:43:20 -0500

    feat: add scripts to assist in Dallas automation

---
+++ Thompson.RecordSearch.Utility/Classes/DallasScriptHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thompson.RecordSearch.Utility.Classes
+{
+    public static class DallasScriptHelper
+    {
+
+        public static string GetScriptContent
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(scriptContent)) return scriptContent;
+                scriptContent = ScriptContent();
+                return scriptContent;
+            }
+        }
+        public static string GetScriptFileName
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(scriptFileName)) return scriptFileName;
+                scriptFileName = ScriptFileName();
+                return scriptFileName;
+            }
+        }
+        private static string scriptFileName;
+        private static string scriptContent;
+        private static string ScriptFileName()
+        {
+            var appFolder =
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var xmlFolder = Path.Combine(appFolder, "xml");
+            var scriptFile = Path.Combine(xmlFolder, "dallasScriptingHelp.txt");
+            return scriptFile;
+        }
+
+        private static string ScriptContent()
+        {
+            var content = File.ReadAllText(GetScriptFileName);
+            content = content.Replace('\\', '~').Replace("~~", "~");
+            return content;
+        }
+    }
+}
---
+++ UnitTests/Thompson.RecordSearch.Utility.UnitTests/Dallas/ScriptingTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Thompson.RecordSearch.Utility.Classes;
+
+namespace Thompson.RecordSearch.Utility.UnitTests.Dallas
+{
+    [TestClass]
+    public class ScriptingTests
+    {
+        [TestMethod]
+        public void DoesScriptFileExist()
+        {
+            var obj = DallasScriptHelper.GetScriptFileName;
+            var exists = File.Exists(obj);
+            Assert.IsTrue(exists);
+        }
+
+        [TestMethod]
+        public void CanGetScriptContent()
+        {
+            var obj = DallasScriptHelper.GetScriptContent;
+            Assert.IsNotNull(obj);
+        }
+
+        [TestMethod]
+        public void CanGetScriptFileName()
+        {
+            var obj = DallasScriptHelper.GetScriptFileName;
+            Assert.IsNotNull(obj);
+        }
+    }
+}
