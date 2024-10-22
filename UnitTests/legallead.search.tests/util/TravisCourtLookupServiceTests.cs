245c413a8e4d53fd12169c525715bbb16929e504
Frank Thompson <frank.thompson.jr@gmail.com>
2024-10-22 10:38:45 -0500

    test: adding travis code coverage

---
+++ UnitTests/legallead.search.tests/util/TravisCourtLookupServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using LegalLead.PublicData.Search.Util;
+
+namespace legallead.search.tests.util
+{
+    public class TravisCourtLookupServiceTests
+    {
+        [Theory]
+        [InlineData("Justice", "")]
+        [InlineData("Justice", "Precinct 1")]
+        [InlineData("Justice", "Precinct 2")]
+        [InlineData("Justice", "Precinct 3")]
+        [InlineData("Justice", "Precinct 4")]
+        [InlineData("Justice", "Precinct 5")]
+        public void ServiceCanGetAddress(string courtType, string court)
+        {
+            var address = TravisCourtLookupService.GetAddress(courtType, court);
+            Assert.False(string.IsNullOrEmpty(address));
+        }
+    }
+}
