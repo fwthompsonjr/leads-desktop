c7db4fe8991e7eede2df78d1dd69239e0ec67220
Frank Thompson <frank.thompson.jr@gmail.com>
2024-11-18 17:16:28 -0600

    feat: set method to write usage data

---
+++ LegalLead.PublicData.Search/Helpers/SessionMonthToDatePersistence.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Windows.Documents;
+using Thompson.RecordSearch.Utility.Extensions;
+using Thompson.RecordSearch.Utility.Models;
+
+namespace LegalLead.PublicData.Search.Helpers
+{
+
+    public class SessionMonthToDatePersistence : SessionFilePersistence
+    {
+        protected SessionUsageReader UsagePersistence
+        {
+            get
+            {
+                if (_usagePersistence != null) return _usagePersistence;
+                _usagePersistence = SessionPersistenceContainer
+                    .GetContainer
+                    .GetInstance<SessionUsageReader>();
+                return _usagePersistence;
+            }
+            set { _usagePersistence = value; }
+        }
+
+        private SessionUsageReader _usagePersistence = null;
+        public void WriteUserRecord()
+        {
+            var now = DateTime.UtcNow.Date;
+            int months = 12;
+            var list = new List<UsageHistoryModel>();
+            var indexes = Enum.GetValues<SourceType>()
+                .Select(s => (int)s).ToList();
+            while (months >= 0)
+            {
+                indexes.ForEach(i =>
+                {
+                    list.AddRange(UsagePersistence.GetUsage(now.AddMonths(-months)));
+                });
+                months--;
+            }
+            var json = JsonConvert.SerializeObject(list);
+            Write(json);
+        }
+
+        public int GetCount(string countyName)
+        {
+            var text = Read();
+            if (string.IsNullOrEmpty(text)) return -1;
+
+            return 0;
+        }
+
+        protected override string SetupFile
+        {
+            get
+            {
+                if (setupFileName != null) return setupFileName;
+                setupFileName = SessionUtil.GetFullFileName(datFileName);
+                return setupFileName;
+            }
+        }
+        private static string setupFileName = null;
+        private const string datFileName = "session.mtd";
+    }
+}
