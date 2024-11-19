e0771e30a0ae01985ecf459f0aee55e12ce2dc71
Frank Thompson <frank.thompson.jr@gmail.com>
2024-11-19 15:12:33 -0600

    fix: add data binding for user county credential

--- LegalLead.PublicData.Search/FsCountySetting.cs
+++ LegalLead.PublicData.Search/FsCountySetting.cs
@@ -1,7 +1,11 @@
 ﻿using LegalLead.PublicData.Search.Common;
 using LegalLead.PublicData.Search.Extensions;
+using LegalLead.PublicData.Search.Helpers;
+using LegalLead.PublicData.Search.Interfaces;
+using StructureMap.Pipeline;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -18,17 +22,21 @@ namespace LegalLead.PublicData.Search
                 var id = (int)type;
                 list.Add(new() { CountyId = id, CountyName = name });
             });
+            var allowAllCounties = IsAllCountyEnabled();
             list.Sort((a, b) => a.CountyName.CompareTo(b.CountyName));
             list.ForEach(itm =>
             {
+                var setting = GetCountyCredential(itm.CountyName);
+                itm.UserName = setting[0];
+                itm.UserPassword = setting[1];
                 if (!vwlist.Exists(x => x.CountyName == itm.CountyName))
                 {
                     vwlist.Add(new CountyDisplayItem
                     {
                         CountyName = itm.CountyName,
-                        IsEnabled = itm.IsEnabled,
+                        IsEnabled = IsCountyEnabled(allowAllCounties, itm.CountyId),
                         UserName = itm.UserName,
-                        UserPassword = itm.UserPassword
+                        UserPassword = GetMasked(itm.UserPassword)
                     });
                 }
             });
@@ -36,6 +44,36 @@ namespace LegalLead.PublicData.Search
 
         }
 
+        private static bool IsAllCountyEnabled()
+        {
+            var webdetail = UserAccountReader.GetAccountPermissions();
+            return webdetail.Equals("-1");
+        }
+
+        private static bool IsCountyEnabled(bool isAllCounty, int countyId)
+        {
+            if (isAllCounty) return true;
+            var webdetail = UserAccountReader.GetAccountPermissions();
+
+            var webid = webdetail.Split(',')
+                .Where(w => { return int.TryParse(w, out var _); })
+                .Select(s => int.Parse(s, CultureInfo.CurrentCulture))
+                .ToList();
+            return webid.Contains(countyId);
+        }
+        private static string[] GetCountyCredential(string county)
+        {
+            const char pipe = '|';
+            var model = UserAccountReader.GetAccountCredential(county);
+            if (string.IsNullOrEmpty(model) || !model.Contains(pipe)) return new[] {"", ""};
+            return model.Split(pipe);
+        }
+        private static string GetMasked(string str)
+        {
+            if (string.IsNullOrEmpty (str)) return "";
+            var len = str.Length;
+            return string.Concat(Enumerable.Repeat("*", len));
+        }
         private sealed class CountyDisplayItem
         {
 
@@ -48,6 +86,10 @@ namespace LegalLead.PublicData.Search
 
         private readonly List<CountyDisplayItem> vwlist = new();
         private readonly List<CountyPermissionViewModel> list = new();
-        private static readonly List<SourceType> sourceTypes = Enum.GetValues<SourceType>().ToList();
+        private static readonly List<SourceType> sourceTypes = Enum.GetValues<SourceType>().ToList(); 
+        private static ISessionPersistance UserAccountReader =
+            SessionPersistenceContainer
+            .GetContainer
+            .GetInstance<ISessionPersistance>(ApiHelper.ApiMode);
     }
 }
