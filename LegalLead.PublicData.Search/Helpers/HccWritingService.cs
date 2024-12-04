30fab66dafda376d3640581ab3b469ff08572bfe
Frank Thompson <frank.thompson.jr@gmail.com>
2024-12-04 01:06:38 -0600

    feat: create helper to post hcc data

--- LegalLead.PublicData.Search/Helpers/HccWritingService.cs
+++ LegalLead.PublicData.Search/Helpers/HccWritingService.cs
@@ -1,6 +1,43 @@
-﻿namespace LegalLead.PublicData.Search.Helpers
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using Thompson.RecordSearch.Utility.Dto;
+using Thompson.RecordSearch.Utility.Interfaces;
+
+namespace LegalLead.PublicData.Search.Helpers
 {
-    internal class HccWritingService
+    public class HccWritingService
     {
+        public HccWritingService(IHttpService http)
+        {
+            httpService = http;
+        }
+        private readonly IHttpService httpService;
+        public void Write(string csvdata)
+        {
+            var dat = Encoding.UTF8.GetBytes(csvdata);
+            var conversion = Convert.ToBase64String(dat);
+            var payload = new { Content = conversion };
+            using var client = new HttpClient { Timeout = TimeSpan.FromSeconds(30) };
+            _ = httpService.PostAsJson<object, object>(client, RemoteAddress, payload);
+        }
+        private static string RemoteAddress
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(_remoteAddress)) return _remoteAddress;
+                _remoteAddress = GetRemoteAddress();
+                return _remoteAddress;
+            }
+        }
+        private static string _remoteAddress = string.Empty;
+        private static string GetRemoteAddress()
+        {
+            var model = HccConfigurationModel.GetModel().RemoteModel;
+            if (string.IsNullOrEmpty(model.Url) ||
+                string.IsNullOrEmpty(model.FetchUrl))
+                throw new NullReferenceException();
+            return string.Concat(model.Url, model.PostUrl);
+        }
     }
 }
