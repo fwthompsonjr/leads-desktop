3d341ba2599f56236469157c5f8d6b3593139b12
Frank Thompson <frank.thompson.jr@gmail.com>
2025-02-27 19:10:24 -0600

    feat: allow grouping in data viewer

--- LegalLead.PublicData.Search/Classes/PreviewSearchRequestedEvent.cs
+++ LegalLead.PublicData.Search/Classes/PreviewSearchRequestedEvent.cs
@@ -1,6 +1,6 @@
 ﻿using LegalLead.PublicData.Search.Classes;
 using LegalLead.PublicData.Search.Extensions;
-using Polly;
+using LegalLead.PublicData.Search.Models;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -83,8 +83,9 @@ namespace LegalLead.PublicData.Search
             private readonly SearchResult context;
             public PanelManager(
                 Panel source,
-                SearchResult search = null) {
-                viewPanel = source; 
+                SearchResult search = null)
+            {
+                viewPanel = source;
                 context = search;
             }
             public void Unload()
@@ -107,13 +108,15 @@ namespace LegalLead.PublicData.Search
                 // Create the TableLayoutPanel
                 TableLayoutPanel viewPanelTableLayout = new()
                 {
-                    RowCount = 4,
-                    ColumnCount = 2,
+                    RowCount = 5,
+                    ColumnCount = 3,
                     Dock = DockStyle.Fill
                 };
                 // Create button
-                Button returnButton = new() { Text = "Return" };
+                Button returnButton = new() { Text = "Return", AutoSize = true };
+                Button toggleButton = new() { Text = "Group", Tag = "Off", AutoSize = true };
                 returnButton.Click += ReturnButton_Click;
+                toggleButton.Click += ToggleButton_Click;
                 // Create labels 
                 Label vwLabel01A = new() { Text = "Website", AutoSize = true };
                 Label vwLabel01B = new() { Text = context.Website, AutoSize = true };
@@ -125,7 +128,7 @@ namespace LegalLead.PublicData.Search
                 Label vwLabel04B = new() { Text = $"{context.AddressList.Count}", AutoSize = true };
                 var collection = new List<LabelPair>
                 {
-                    new(){ Caption = returnButton, Content = null },
+                    new(){ Caption = returnButton, Content = toggleButton },
                     new(){ Caption = vwLabel01A, Content = vwLabel01B },
                     new(){ Caption = vwLabel02A, Content = vwLabel02B },
                     new(){ Caption = vwLabel03A, Content = vwLabel03B },
@@ -138,6 +141,7 @@ namespace LegalLead.PublicData.Search
                 }
                 viewPanelTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
                 viewPanelTableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 90F));
+                viewPanelTableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 90F));
                 viewPanelTableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
 
                 // Add labels to the TableLayoutPanel
@@ -145,21 +149,24 @@ namespace LegalLead.PublicData.Search
                 {
                     var current = collection[i];
                     viewPanelTableLayout.Controls.Add(current.Caption, 0, i);
-                    if (current.Content == null) continue;
                     viewPanelTableLayout.Controls.Add(current.Content, 1, i);
+                    if (i == 0) continue;
+                    viewPanelTableLayout.SetColumnSpan(current.Content, 2);
                 }
                 if (context.AddressList != null && context.AddressList.Count > 0)
                 {
                     // Create and add the DataGridView
                     DataGridView viewPanelDataGrid = new()
                     {
+                        Name = "viewPanelDataGrid",
                         Dock = DockStyle.Fill,
                         Padding = new Padding(5)
                     };
+                    viewPanelDataGrid.Tag = context.AddressList;
                     context.AddressList.BindGrid(viewPanelDataGrid);
-                    // Add the DataGridView to the TableLayoutPanel with RowSpan = 2
+                    // Add the DataGridView to the TableLayoutPanel with ColumnSpan = 2
                     viewPanelTableLayout.Controls.Add(viewPanelDataGrid, 0, collection.Count);
-                    viewPanelTableLayout.SetColumnSpan(viewPanelDataGrid, 2);
+                    viewPanelTableLayout.SetColumnSpan(viewPanelDataGrid, 3);
                 }
                 // Add the TableLayoutPanel to the viewPanel
                 viewPanel.Controls.Add(viewPanelTableLayout);
@@ -180,13 +187,51 @@ namespace LegalLead.PublicData.Search
                 }
             }
 
+            private static void ToggleButton_Click(object sender, System.EventArgs e)
+            {
+                if (sender is not Button button) return;
+                if (button.Tag is not string toggleMode) return;
+                if (button.FindForm() is not FormMain main) return;
+                var grids = main.Controls.Find("viewPanelDataGrid", true);
+                if (grids == null || grids.Length == 0) return;
+                var src = grids[0];
+                if (src is not DataGridView grid) return;
+                if (grid.Tag is not List<QueryDbResponse> query) return;
+                var isGrouped = toggleMode != "Off";
+                ToggleGrouping(grid, query, isGrouped);
+                button.Tag = isGrouped ? "Off" : "On";
+            }
+
+            private static void ToggleGrouping(DataGridView grid, List<QueryDbResponse> query, bool isGrouped)
+            {
+                grid.Columns.Clear();
+                if (isGrouped)
+                {
+                    grid.DataSource = query;
+                    grid.Refresh();
+                    return;
+                }
+                var groupedData = query.GroupBy(c => new { c.Court, c.DateFiled })
+                       .Select(g => new
+                       {
+                           g.Key.Court,
+                           g.Key.DateFiled,
+                           Count = g.Count()
+                       }).ToList();
+                groupedData.Sort((a, b) =>
+                {
+                    var aa = a.Court.CompareTo(b.Court);
+                    if (aa != 0) return aa;
+                    return a.DateFiled.CompareTo(b.DateFiled);
+                });
+                grid.DataSource = groupedData;
+                grid.Refresh();
+            }
             private class LabelPair
             {
                 public Control Caption { get; set; }
                 public Control Content { get; set; }
             }
         }
-
-
     }
 }
